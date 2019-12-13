41f4612e66cdd93a85f20f44b7a0fd615ee68c43
Arseniy <arseniyoguzov@gmail.com>
2019-12-13 18:54:24 +0300

    adaptive for carousel x2

--- src/components/Contacts/script.js
+++ src/components/Contacts/script.js
@@ -1,19 +1,19 @@
-import placemarkIcon from "/components/Contacts/assets/placemark.svg"
-
-ymaps.ready(init);
-
-function init(){
-    var myMap = new ymaps.Map("contacts-map", {
-        center: [51.671929, 39.194751],
-        zoom: 18
-    });
-
-    var placemark = new ymaps.Placemark([51.671753, 39.196515], {}, {
-        iconLayout: 'default#image',
-        iconImageHref: placemarkIcon,
-        iconImageSize: [50, 50],
-    });
-
-
-    myMap.geoObjects.add(placemark);
-}
+// import placemarkIcon from "/components/Contacts/assets/placemark.svg"
+//
+// ymaps.ready(init);
+//
+// function init(){
+//     var myMap = new ymaps.Map("contacts-map", {
+//         center: [51.671929, 39.194751],
+//         zoom: 18
+//     });
+//
+//     var placemark = new ymaps.Placemark([51.671753, 39.196515], {}, {
+//         iconLayout: 'default#image',
+//         iconImageHref: placemarkIcon,
+//         iconImageSize: [50, 50],
+//     });
+//
+//
+//     myMap.geoObjects.add(placemark);
+// }
